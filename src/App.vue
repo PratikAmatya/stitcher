feb36badd4615ba83954de78a9b6a54c794ddfe1
Pratik <pratik@beamlab.co>
2024-06-14 17:56:32 +0545

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-main class="main">
+      <SubmitForm />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import SubmitForm from "@/components/SubmitForm.vue";
+
+export default {
+  name: "SubmitForm",
+  components: {
+    SubmitForm,
+  },
+};
+</script>
+
+<style>
+.main {
+  background-image: linear-gradient(
+    315deg,
+    hsl(5deg 100% 74%) 0%,
+    hsl(334deg 96% 80%) 30%,
+    hsl(303deg 89% 85%) 46%,
+    hsl(271deg 74% 91%) 63%,
+    hsl(240deg 11% 96%) 100%
+  );
+}
+</style>
